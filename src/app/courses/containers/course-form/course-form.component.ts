84dafc6d8b0c83f417db4a9c1895f47768e090f3
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-07-28 10:54:33 -0300

    FormArray de Lessons

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,12 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from '../../services/courses.service';
 import { Course } from '../../model/course';
+import { Lesson } from '../../model/lesson';
 
 @Component({
   selector: 'app-course-form',
@@ -14,7 +15,10 @@ import { Course } from '../../model/course';
 })
 export class CourseFormComponent implements OnInit {
 
-form = this.formBuilder.group({
+  //Inicializa a variavel sem precisar declará-la
+  form!: FormGroup;
+
+/*form = this.formBuilder.group({
   // Sem uso do NonNullableFormBuilder name: new FormControl<string>('', {nonNullable: true}),
   _id: [''],
   name: [
@@ -23,7 +27,7 @@ form = this.formBuilder.group({
     Validators.minLength(5),
     Validators.maxLength(100)]],
   category: ['', Validators.required],
-});;
+});*/
 
   constructor(
     private formBuilder: NonNullableFormBuilder,
@@ -35,12 +39,48 @@ form = this.formBuilder.group({
   }
 
   ngOnInit(): void {
+    //Caso venha vazio o course.resolver já faz esse serviço
     const course: Course = this.activatedRoute.snapshot.data['course'];
-    this.form.setValue({
+    this.form = this.formBuilder.group({
+      _id: [course._id],
+      name: [course.name, [Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)]],
+      category: [course.category, [Validators.required]],
+      lessons: this.formBuilder.array(this.retrieveLessons(course))
+    });
+    console.log(this.form);
+    console.log(this.form.value);
+
+    /*this.form.setValue({
       _id: course._id,
       name: course.name,
       category: course.category,
-    })
+    });
+    console.log(course);*/
+  }
+
+  private retrieveLessons(course: Course) {
+    const lessons = [];
+    if(course?.lessons) {
+      course.lessons.forEach(lesson =>
+        lessons.push(this.createLesson(lesson)));
+    } else {
+      lessons.push(this.createLesson());
+    }
+    return lessons;
+  }
+
+  private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}) {
+    return this.formBuilder.group({
+      id: [lesson.id],
+      name: [lesson.name],
+      youtubeUrl: [lesson.youtubeUrl]
+    });
+  }
+
+  getLessonsFormArray() {
+    return (<UntypedFormArray>this.form.get('lessons')).controls;
   }
 
   onSubmit() {
--- src/app/courses/model/course.ts
+++ src/app/courses/model/course.ts
@@ -1,5 +1,9 @@
+import { Lesson } from "./lesson";
+
 export interface Course {
   _id: string;
   name: string;
   category: string;
+  // Operador Elvis por conta do formato ?:, funciona mesmo se lessons for nulo
+  lessons?: Lesson[];
 }
